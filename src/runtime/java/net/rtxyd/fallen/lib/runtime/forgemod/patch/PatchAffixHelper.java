b0cbccb6c9e2d32a9a454fa39a73e5e5428191fe
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-04-22 20:31:05 +0800

    Patch AffixHlepr with FallenPatch

---
+++ src/runtime/java/net/rtxyd/fallen/lib/runtime/forgemod/patch/PatchAffixHelper.java
@@ -0,0 +1,58 @@
+package net.rtxyd.fallen.lib.runtime.forgemod.patch;
+
+import dev.shadowsoffire.apotheosis.adventure.affix.AffixHelper;
+import net.rtxyd.fallen.lib.api.IFallenPatch;
+import net.rtxyd.fallen.lib.api.annotation.FallenPatch;
+import net.rtxyd.fallen.lib.api.annotation.Targets;
+import net.rtxyd.fallen.lib.runtime.forgemod.patch.inserters.Inserters;
+import net.rtxyd.fallen.lib.type.service.IFallenPatchContext;
+import net.rtxyd.fallen.lib.type.service.IFallenPatchCtorContext;
+import net.rtxyd.fallen.lib.type.service.IPatchDescriptor;
+import net.rtxyd.fallen.lib.util.PatchUtil;
+import net.rtxyd.fallen.lib.util.patch.InserterKey;
+import net.rtxyd.fallen.lib.util.patch.InserterMethodData;
+import net.rtxyd.fallen.lib.util.patch.InserterType;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+@FallenPatch(
+        targets = @Targets(
+                exact = {AffixHelper.class}),
+        inserters = {Inserters.class}
+)
+public class PatchAffixHelper implements IFallenPatch{
+    private final IPatchDescriptor desc;
+
+    public PatchAffixHelper(IFallenPatchCtorContext iFallenPatchCtorContext) {
+        desc = iFallenPatchCtorContext.currentPatch();
+    }
+
+    @Override
+    public void apply(IFallenPatchContext ctx) {
+        ClassNode cn = ctx.getClassNode();
+        InserterMethodData hookData = ctx.getFallenInserter(
+                InserterKey.of("net.rtxyd.fallen.lib.runtime.forgemod.patch.inserters.Inserters",
+                        "hookAffixApply",
+                        InserterType.BEFORE_MODIFY_ARG));
+        if (hookData == null) return;
+        for (MethodNode method : cn.methods) {
+            InsnList insns = method.instructions;
+            // we must ensure the record class is clean, or it could be risky when we create instance.
+            if (insns == null) continue;
+            if (!(method.name.equals("applyAffix") && method.desc.equals("(Lnet/minecraft/world/item/ItemStack;Ldev/shadowsoffire/apotheosis/adventure/affix/AffixInstance;)V"))) continue;
+            // only patches like Map.get(key), or Map.getOrDefault(key),
+            // and there are other cases, but will be strictly filtered.
+            PatchUtil.insertMethodHook(cn,
+                    method,
+                    m ->
+                            m.getOpcode() == Opcodes.INVOKEVIRTUAL
+                            && m instanceof MethodInsnNode mn
+                            && mn.owner.endsWith("Map")
+                            && mn.name.equals("put"),
+                    hookData);
+        }
+    }
+}
---
+++ src/runtime/java/net/rtxyd/fallen/lib/runtime/forgemod/patch/inserters/Inserters.java
@@ -0,0 +1,30 @@
+package net.rtxyd.fallen.lib.runtime.forgemod.patch.inserters;
+
+import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
+import dev.shadowsoffire.apotheosis.adventure.affix.AffixInstance;
+import dev.shadowsoffire.placebo.reload.DynamicHolder;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.rtxyd.fallen.lib.api.annotation.FallenInserter;
+import net.rtxyd.fallen.lib.api.annotation.Notes;
+import net.rtxyd.fallen.lib.api.annotation.Params;
+import net.rtxyd.fallen.lib.runtime.forgemod.FallenLib;
+import net.rtxyd.fallen.lib.runtime.forgemod.addon.apotheosis.event.AffixApplyEvent;
+import net.rtxyd.fallen.lib.type.util.patch.IInserterContext;
+import net.rtxyd.fallen.lib.util.INull;
+import net.rtxyd.fallen.lib.util.patch.InserterType;
+
+import java.util.HashMap;
+
+public class Inserters {
+    @FallenInserter(type = InserterType.BEFORE_MODIFY_ARG, params = @Params(catchOuterArgs = {0}, modifyArg = 1))
+    @Notes(argTypes = {ItemStack.class, DynamicHolder.class, AffixInstance.class}, receiverType = HashMap.class, retType = {})
+    // 0 is item, 2 is affix instance
+    // Map.put(DynamicHolder<? extends Affix>, AffixInstance)
+    // java/util/HashMap.put (Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;
+    public static AffixInstance hookAffixApply(IInserterContext<HashMap<DynamicHolder<? extends Affix>, AffixInstance>, INull> ctx, Object... args) {
+        AffixApplyEvent event = new AffixApplyEvent((ItemStack) args[0], (AffixInstance) args[2]);
+        MinecraftForge.EVENT_BUS.post(event);
+        return event.getAffix();
+    }
+}
