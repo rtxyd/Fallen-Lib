e10c853a7b3962b6d2cc1b2f6a01771ecbff54ab
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-07-10 22:49:08 +0800

    remove unused class

--- src/runtime/java/net/rtxyd/fallen/lib/runtime/forgemod/mixin/CodecProviderMixin.java
+++
@@ -1,9 +0,0 @@
-//package net.rtxyd.fallen.lib.runtime.forgemod.mixin;
-//
-//import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
-//import dev.shadowsoffire.placebo.codec.CodecProvider;
-//import net.rtxyd.fallen.lib.runtime.forgemod.util.ICodecProvider;
-//import org.spongepowered.asm.mixin.Mixin;
-//
-//@Mixin(value = CodecProvider.class, remap = false)
-//public interface CodecProviderMixin extends ICodecProvider<GemBonus> {}
